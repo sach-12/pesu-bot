add7303356a1b5265a428ea55c2fb8ce51c86733
ArvindAROO <i.arvindkrishna@gmail.com>
2021-05-10 09:11:56 +0530

    added restart command

--- cogs/beta.py
+++ cogs/beta.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 # from discord.utils import get
 import os
 from dotenv import load_dotenv
+import subprocess
+import sys
 
 load_dotenv('.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -21,12 +23,22 @@ class beta(commands.Cog):
     # This file will be used for obsolete code(if needed to be stored) or any other function
     # to be tested before moving to another cog
 
-
     @commands.Cog.listener()
     async def on_ready(self):
         await self.client.wait_until_ready()
         self.guildObj = self.client.get_guild(GUILD_ID)
 
+    @commands.command(aliases=['restart'])
+    async def _restart(self, ctx):
+        BOT_TEST = 749473757843947671
+        if ctx.author.id == 723377619420184668 or ctx.author.id == 718845827413442692:
+            with open('cogs/verified.csv', 'r') as fp:
+                await self.client.get_channel().send(file=discord.File(fp, 'verified.csv'))
+            fp.close()
+            p = subprocess.Popen(['python', 'start.py'])
+            sys.exit(0)
+
+    
     #     self.admin = get(self.guildObj.roles, id=742800061280550923)
     #     self.bot_devs = get(self.guildObj.roles, id=750556082371559485)
 
---
+++ start.py
@@ -0,0 +1,5 @@
+import sys
+import subprocess
+
+p = subprocess.Popen(['python', 'bot.py'])
+sys.exit(0)
