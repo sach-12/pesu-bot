e85f720cc644f6d5cb74589b33876bc1131a79ad
Space-Gamer <spacegamer1111@gmail.com>
2023-08-26 15:02:02 +0530

    mod.py + Added logging in `mod_logs` for `untimeout` command. + `lock` command to lock channel added. The action will also be logged to the `mod_logs` channel. + `unlock` command to unlock channel added. It has a parameter `private` which should be `True` for private channels. The action will also be logged to the `mod_logs` channel. + `echo` command to echo messages in any channel. Parameter `anonymous` to send the message anonymously. The action will also be logged to the `mod_logs` channel.

--- src/cogs/interactions/slash/mod.py
+++ src/cogs/interactions/slash/mod.py
@@ -100,6 +100,115 @@ class SlashMod(commands.Cog):
             await interaction.followup.send(f"Failed due to following error:\n`{e}`")
             return
         await interaction.followup.send(f"Successfully removed timeout for {member.mention}")
+        # Send to logs
+        embed = discord.Embed(title="Timeout removed",
+                              description=f"{member.name}'s timeout was removed by {interaction.user.mention}",
+                              colour=0xffff00, timestamp=discord.utils.utcnow())
+        embed.set_footer(text=f"Removed by {interaction.user.name}")
+        await member.guild.get_channel_or_thread(self.client.config['channels']['mod_logs']).send(embed=embed)
+
+    @mod_group.command(name='lock', description='To lock a channel')
+    @app_commands.describe(channel="The channel to be locked", reason="The reason for locking the channel (optional)")
+    async def lock(self, interaction: discord.Interaction, channel: discord.TextChannel = None, reason: str = None):
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        # Check if the user not an admin or mod
+        if not self.check_admin_mod(interaction.user):
+            await interaction.followup.send(content="Noob you can't do that", ephemeral=True)
+            return
+        if channel is None:
+            channel = interaction.channel
+        for obj in channel.overwrites:
+            if isinstance(obj, discord.Role) and obj.id in [self.client.config['roles']['admin'],
+                                                            self.client.config['roles']['mod']]:
+                continue
+            try:
+                perms = channel.overwrites_for(obj)
+                perms.send_messages = False
+                await channel.set_permissions(obj, overwrite=perms, reason=reason)
+            except Exception as e:
+                await interaction.followup.send(f"Failed for {obj.mention} due to following error:\n`{e}`")
+                continue
+        await interaction.followup.send(f"Successfully locked {channel.mention}")
+        # Send channel locked message
+        embed = discord.Embed(title="Channel locked :lock:",
+                              description=f"{'**Reason:** '+reason if reason else 'Reason not specified.'}",
+                              colour=0xffa500, timestamp=discord.utils.utcnow())
+        await channel.send(embed=embed)
+        # Send to logs
+        embed = discord.Embed(title="Channel locked",
+                              description=f"{channel.mention} was locked by {interaction.user.mention}\n\n**Reason**: "
+                                          f"{reason if reason else 'Reason not specified.'}",
+                              colour=0xffa500, timestamp=discord.utils.utcnow())
+        embed.set_footer(text=f"Locked by {interaction.user.name}")
+        await channel.guild.get_channel_or_thread(self.client.config['channels']['mod_logs']).send(embed=embed)
+
+    @mod_group.command(name='unlock', description='To unlock a channel')
+    @app_commands.describe(channel="The channel to be unlocked", private_channel="Whether the channel is private")
+    async def unlock(self, interaction: discord.Interaction, channel: discord.TextChannel = None,
+                     private_channel: bool = True):
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        # Check if the user not an admin or mod
+        if not self.check_admin_mod(interaction.user):
+            await interaction.followup.send(content="Noob you can't do that", ephemeral=True)
+            return
+        if channel is None:
+            channel = interaction.channel
+        for obj in channel.overwrites:
+            if private_channel and isinstance(obj, discord.Role) and obj.id == channel.guild.default_role.id:
+                continue
+            elif isinstance(obj, discord.Role) and obj.name.lower() == 'muted':
+                continue
+            try:
+                perms = channel.overwrites_for(obj)
+                perms.send_messages = True
+                await channel.set_permissions(obj, overwrite=perms)
+            except Exception as e:
+                await interaction.followup.send(f"Failed for {obj.mention} due to following error:\n`{e}`")
+                continue
+        await interaction.followup.send(f"Successfully unlocked {channel.mention}")
+        # Send channel unlocked message
+        embed = discord.Embed(title="Channel unlocked :unlock:",
+                              colour=0x00ff00, timestamp=discord.utils.utcnow())
+        await channel.send(embed=embed)
+        # Send to logs
+        embed = discord.Embed(title="Channel unlocked",
+                              description=f"{channel.mention} was unlocked by {interaction.user.mention}",
+                              colour=0x00ff00, timestamp=discord.utils.utcnow())
+        embed.set_footer(text=f"Unlocked by {interaction.user.name}")
+        await channel.guild.get_channel_or_thread(self.client.config['channels']['mod_logs']).send(embed=embed)
+
+    @mod_group.command(name='echo', description='To echo a message')
+    @app_commands.describe(channel="The channel to send the message in", message="The message to be sent (Use \\n for "
+                                                                                 "new line)",
+                           anonymous="Whether the message is to be sent anonymously")
+    async def echo(self, interaction: discord.Interaction, channel: discord.TextChannel, message: str,
+                   anonymous: bool = False):
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        # Check if the user not an admin or mod
+        if not self.check_admin_mod(interaction.user):
+            await interaction.followup.send(content="Noob you can't do that", ephemeral=True)
+            return
+        try:
+            message = message.replace('\\n', '\n')
+            if anonymous:
+                await channel.send(message)
+            else:
+                webhooks = await channel.webhooks()
+                webhook = discord.utils.get(webhooks, name="Echo")
+                if webhook is None:
+                    webhook = await channel.create_webhook(name="Echo")
+                await webhook.send(message, username=interaction.user.global_name,
+                                   avatar_url=interaction.user.avatar.url)
+        except Exception as e:
+            await interaction.followup.send(f"Failed due to following error:\n`{e}`")
+            return
+        await interaction.followup.send(f"Successfully echoed the message in {channel.mention}")
+        # Send to logs
+        embed = discord.Embed(title="Message echoed",
+                              description=f"**Anonymous**: {anonymous}\n\n**Message**:\n{message}",
+                              colour=0x0000ff, timestamp=discord.utils.utcnow())
+        embed.set_footer(text=f"Echoed by {interaction.user.name}")
+        await channel.guild.get_channel_or_thread(self.client.config['channels']['mod_logs']).send(embed=embed)
 
 
 async def setup(client: commands.Bot):
